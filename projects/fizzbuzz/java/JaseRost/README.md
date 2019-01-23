b40a59b5ffe714c2825c3c31c8056754a7e1b066
Jase Rost <jaserost@yahoo.com>
2019-01-23 05:22:48 -0700

    Created project and README

---
+++ projects/fizzbuzz/java/JaseRost/README.md
@@ -0,0 +1,8 @@
+In order to run this program you must:
+
+1.	Ensure you have the correct PATH variables set up if on PC.
+
+2.	Navigate to this folder and compile the java class using javac.
+
+3.	Run the program using "java Rost n" where n is the number you would like the program
+	to count up to.
---
+++ projects/fizzbuzz/java/JaseRost/Rost.java
@@ -0,0 +1,18 @@
+public class Rost {
+
+    public static void main(String args[]) {
+        int num = Integer.valueOf(args[0]);
+
+        for (int i = 1; i <= num; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
