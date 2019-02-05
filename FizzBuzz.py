50b64ee43c7497d96e5667a95decdd91dae8a277
Kala <michaela_lightner@yahoo.com>
2019-02-04 17:46:55 -0700

    Project 2

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+print('Enter number:')
+n = input()
+n = int(n)
+for num in range(1, n):
+    if num % 3 == 0 and num % 5 == 0:
+        print('FizzBuzz')
+    elif num % 3 == 0:
+        print('Fizz')
+    elif num % 5 == 0:
+        print('Buzz')
+    else:
+        print(num)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # methods
 Supporting material for the Industry Methods seminar at Montana State University
+
+* Open Python
+* Run program
+* Follow prompt to enter number 
