cabf3fd65714bf286c312a136f0fba6b3ec8a4f0
jthompson22 <36999434+jthompson22@users.noreply.github.com>
2019-02-04 18:03:52 -0700

    Update python_jaredthompson.py

--- projects/fizzbuzz/python/python_jaredthompson.py
+++ projects/fizzbuzz/python/python_jaredthompson.py
@@ -1,7 +1,7 @@
 def function1(num):
     for i in range(1,num+1):
         if (i%3==0 and i%5==0):
-            print("fizz" + "buzz")
+            print("fizzbuzz")
         elif i%3==0:
             print("fizz")
         elif i%5==0:
@@ -9,9 +9,13 @@ def function1(num):
         else:
             print(i)
 
-n = 17
-l = 7
+def main():
+    print("type in a value:")
+    i = int(input())
+    print(i)
+    if (i > 0 and i < 100):
+         function1(i)
+    else:
+        print("This value is not allowed")
 
-function1(n)
-print()
-function1(l)
+main()
