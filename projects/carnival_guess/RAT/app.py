aa0694900b9e83d94babe4ed67f95787f2a85a8a
Tim Parrish <timothyjparrish@gmail.com>
2019-04-01 15:26:25 -0600

    testing

--- projects/carnival_guess/RAT/app.py
+++ projects/carnival_guess/RAT/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 toGuess = random.randint(0,100)
 print("The number to guess is: " + str(toGuess))
 
-@app.route('/<int:myVar>')
+@app.route('www.timothyparrish.com/html/carival.html?<int:myVar>')
 def guesser(myVar):
 	hot_cold = "Way out of the ballpark."
 	if myVar == toGuess:
@@ -15,19 +15,19 @@ def guesser(myVar):
 		close = "You were a bit too high"
 	else:
 		close = "You were a bit too low"
-	return render_template('home.html', hot_cold = close)
+	return render_template('www.timothyparrish.com/html/carival.html', hot_cold = close)
 
-@app.route('/<float:myVar>')#error handling for float input
+@app.route('www.timothyparrish.com/html/carival.html?<float:myVar>')#error handling for float input
 def errorFloat(myVar):
-	return render_template('home.html', hot_cold = "Please enter and integer value")
+	return render_template('www.timothyparrish.com/html/carival.html', hot_cold = "Please enter and integer value")
 
-@app.route('/<string:myVar>')#error handling for string input
+@app.route('www.timothyparrish.com/html/carival.html?<string:myVar>')#error handling for string input
 def errorString(myVar):
-	return render_template('home.html', hot_cold = "Please enter and integer value")
+	return render_template('www.timothyparrish.com/html/carival.html', hot_cold = "Please enter and integer value")
 
-@app.route('/')#if no input is given
+@app.route('www.timothyparrish.com/html/carival.html/')#if no input is given
 def empty():
-	return render_template('home.html', hot_cold = "Enjoy Guessing!")
+	return render_template('www.timothyparrish.com/html/carival.html', hot_cold = "Enjoy Guessing!")
 
 if __name__ == '__main__':
 	app.run(debug=True)
