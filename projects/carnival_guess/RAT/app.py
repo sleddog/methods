b1f0b23da9f7ac1a034bb2c050fab0338a7b9589
Alex <alexackerlund@bresnan.net>
2019-04-01 14:39:51 -0600

    added error handling as well changed some variable names to match api specification

--- projects/carnival_guess/RAT/app.py
+++ projects/carnival_guess/RAT/app.py
@@ -3,24 +3,34 @@ import random
 
 app = Flask(__name__)
 
-NumToGuess = random.randint(0,100)
-print("The number to guess is: " + str(NumToGuess))
-@app.route('/<int:guess>')
-def foo(guess):
+toGuess = random.randint(0,100)
+print("The number to guess is: " + str(toGuess))
+
+@app.route('/<int:myVar>')
+def guesser(myVar):
 	hot_cold = "Way out of the ballpark."
-	if guess == NumToGuess:
+	if myVar == toGuess:
 		close = "right on! Great Guessing!"
-	elif guess > NumToGuess:
+	elif myVar > toGuess:
 		close = "You were a bit too high"
 	else:
 		close = "You were a bit too low"
 	return render_template('home.html', hot_cold = close)
 
-@app.route('/')
-def foo2():
+@app.route('/<float:myVar>')#error handling for float input
+def errorFloat(myVar):
+	return render_template('home.html', hot_cold = "Please enter and integer value")
+
+@app.route('/<string:myVar>')#error handling for string input
+def errorString(myVar):
+	return render_template('home.html', hot_cold = "Please enter and integer value")
+
+@app.route('/')#if no input is given
+def empty():
 	return render_template('home.html', hot_cold = "Enjoy Guessing!")
 
 if __name__ == '__main__':
 	app.run(debug=True)
 #debug=True means you don't have to restart the sever
-#everytime you make a change
+#everytime you make a change. Remove this argument before
+#submitting
