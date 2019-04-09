3ac8ffad94756821c7ccd4822f825247855f7d63
ajohnson2019 <austindalejohnson@gmail.com>
2019-04-09 15:11:34 -0600

    Projects 3 and 4

--- projects/fizzbuzz/python/AustinJohnson/fizzbuzz.py
+++ projects/fizzbuzz/python/AustinJohnson/fizzbuzz.py
@@ -1,35 +1,48 @@
-def getInput():
-    inputNumber = -1
-    while inputNumber < 0:
-        try:
-            inputNumber = int(input("Please enter the number you would like to check (0 to quit): "))
-            if inputNumber >= 0:
-                return inputNumber
-            else:
-                print("Not an accepted number, please input a positive integer.")
-        except ValueError:
-            print("Not a valid input. Please enter a positive integer.")
+import sys
 
+## I have added comments to hopefully work for my project 3 grade since nobody
+## else is currently working on these projects anymore.
+
+## Added 7 divisibility and changed num to i to keep better track of iterator.
+## This function was great in my opinion so I didn't change much.
 def fizzBuzz(input):
     string = ''
-    for num in range(1, input+1):
-        if num % 3 == 0:
+    for i in range(1, input+1):
+        if i % 3 == 0:
             string += 'Fizz'
-        if num % 5 == 0:
+        if i % 5 == 0:
             string += 'Buzz'
-        if num % 3 != 0 and num % 5 != 0:
-            string += str(num)
+        if i % 7 == 0:
+            string += 'Bazz'
+        if i % 3 != 0 and i % 5 != 0 and i % 7 != 0:
+            string += str(i)
         string += "\n"
-
     return string
 
-def main():
-    ProgramRun = True
-    while ProgramRun:
-        input = getInput()
-        if input == 0:
-            ProgramRun = False
-            break
-        finalOutput = fizzBuzz(input)
-        print(finalOutput)
-main()
+## I wanted this function to be able to run pytests and to clean it up
+## Therefore I ended up reducing the method from 11 lines to 7.
+## This was done by taking out the try/except in this portion of the program.
+## Cleaned up getInput method
+def getInput(checkNum):
+    if checkNum >= 1:
+        return checkNum
+    elif checkNum == 0:
+        sys.exit()
+    else:
+        return -1
+
+## Added try/except in main to prevent errors when not typing an integer.
+## I felt like it was ugly in my getInput method and I had to reaarange
+## some things to get pytest to work properly on this program.
+if __name__ == '__main__':
+    while True:
+        inputNum = 0
+        try:
+            inputNum = int(input("Please enter the number you would like to check (0 to quit): "))
+        except:
+            print("Not an integer, please input an integer.")
+            continue
+        inputNum = getInput(inputNum)
+        if(inputNum == -1):
+            print("Not an accepted number, please input a positive integer.")
+        print(fizzBuzz(inputNum))
